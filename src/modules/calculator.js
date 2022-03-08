1c3e554086ea7f5901b25c820b25e40246d4eee0
Shishkin Yaroslav <company-rosstan@yandex.ru>
2022-03-08 22:48:38 +0300

    lesson21

--- src/modules/calculator.js
+++ src/modules/calculator.js
@@ -1,5 +1,39 @@
 const calculator = () => {
+    const calcSquare = document.querySelector('.calc-square')
+    const calcCount = document.querySelector('.calc-count')
+    const calcDay = document.querySelector('.calc-day')
 
-    console.log('23 2+3 2++3 2+++3 445 677'.replace(/2\+{2,3}3/, '!'))
+    const formName = document.querySelectorAll('form input[type=text]')
+    const formMail = document.querySelectorAll('form input[type=email]')
+    const formPhone = document.querySelectorAll('form input[type=tel]')
+
+    console.log(calcSquare.value)
+
+    calcSquare.addEventListener('input', () => {
+        calcSquare.value = calcSquare.value.replace(/\D/g, "")
+    })
+    calcCount.addEventListener('input', () => {
+        calcCount.value = calcCount.value.replace(/\D/g, "")
+    })
+    calcDay.addEventListener('input', () => {
+        calcDay.value = calcDay.value.replace(/\D/g, "")
+    })
+
+    for (let i = 0; i < formName.length; i++) {
+        formName[i].addEventListener('input', () => {
+            formName[i].value = formName[i].value.replace(/[^а-яА-ЯЁё\-\s]/g, "")
+        })
+    }
+
+    for (let i = 0; i < formMail.length; i++) {
+        formMail[i].addEventListener('input', () => {
+            formMail[i].value = formMail[i].value.replace(/[^a-zA-Z0-9@_.!~*'\-]/g, "")
+        })
+    }
+    for (let i = 0; i < formPhone.length; i++) {
+        formPhone[i].addEventListener('input', () => {
+            formPhone[i].value = formPhone[i].value.replace(/[^0-9()\-]/, "")
+        })
+    }
 }
 export default calculator
