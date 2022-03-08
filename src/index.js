e03362818a856defcd80aab38c15daf3de12b958
Shishkin Yaroslav <company-rosstan@yandex.ru>
2022-03-08 12:33:50 +0300

    mission21

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
+import calculator from "./modules/calculator";
 
 timer('08 march 2022 22:16')
 menu()
 modal()
+calculator()
---
+++ src/modules/calculator.js
@@ -0,0 +1,5 @@
+const calculator = () => {
+
+    console.log('aa aba abba abbba abca abea'.replace(/ab.a/g, '!'))
+}
+export default calculator
