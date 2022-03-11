1928c37253b60597bc6fe410e06b8c03c4a82943
Shishkin Yaroslav <company-rosstan@yandex.ru>
2022-03-11 19:31:54 +0300

    lesson23 Start

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from "./modules/menu";
 import modal from "./modules/modal";
 import calculator from "./modules/calculator";
 import tabs from "./modules/tabs";
+import slider from "./modules/slider";
 
 timer('15 march 2022 22:16')
 menu()
 modal()
 calculator()
 tabs()
+slider()
