f2e10bf75a258cdda6854f7ca113a2b2ac05a723
Yroslav Shishkin <company-rosstan@yandex.ru>
2022-03-16 00:48:55 +0300

    lesson27

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import calculator from "./modules/calculator";
 import tabs from "./modules/tabs";
 import slider from "./modules/slider";
 import calc from "./modules/calc";
+import sandForm from "./modules/sandForm";
+import sendForm from "./modules/sandForm";
 
 timer('15 march 2022 22:16')
 menu()
@@ -13,3 +15,9 @@ calculator()
 tabs()
 slider()
 calc(100)
+sendForm({formId: "form1", someElem: [
+        {
+            type: 'block',
+            id: 'total'
+        }
+    ]})
